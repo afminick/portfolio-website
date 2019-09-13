8193d32d910ddd5805c89c9fa2d5c6640277301f
afminick <afminick@gmail.com>
2019-09-13 21:41:07 +0700

    Started Footer

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css";
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <p>AMWD</p> 
+            <div id='footerContact'>
+                <p>email</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
