920ea9b69a40f7fa81df2b4819d1a86b978d6d7f
afminick <afminick@gmail.com>
2019-09-27 08:37:46 +0700

    Worked on project tiles

--- src/Projects.js
+++ src/Projects.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react';
+import './Projects.css';
 
 function Projects() {
 
     return (
         <div id='projects'>
             <h3>React</h3>
+            <div className="projectsSub">
+                <a className="projectLink" href="https://goofy-montalcini-2ef709.netlify.com/">Hangman</a>
+                <a className="projectLink" href="https://vigilant-lichterman-f14f98.netlify.com/">Vocabulary Tic-Tac-Toe</a>
+            </div>
             <h3>D3 Data visualization</h3>
         </div>
     )
